30354f80412cec052c187a475f12d23303e892af
HuoJia <hjia@curietech.net>
2024-04-10 18:27:07 +0800

    task2完成

--- BackPropogation/task2/task2.py
+++ BackPropogation/task2/task2.py
@@ -5,22 +5,28 @@ train_images, train_labels = load_mnist_train()
 test_images, test_labels = load_mnist_test()
 
 # Initialize parameters
-epochs = 30
+epochs = 5
 batch_size = 32
 learning_rate = 0.01
 input_size = 28 * 28
-hidden_size = 256
+hidden_size1 = 128
+hidden_size2 = 32
 output_size = 10
 
 # three_layer_model = ThreeLayerModel(input_size, hidden_size, output_size)
 two_layer_model = TwoLayerModel(input_size, output_size)
+four_layer_model = FourLayerModel(input_size, hidden_size1, hidden_size2, output_size)
 
 
 def main():
-    train_loss_lst = []
-    test_loss_lst = []
-    train_accuracy_lst = []
-    test_accuracy_lst = []
+    train_loss_lst_2 = []
+    test_loss_lst_2 = []
+    train_accuracy_lst_2 = []
+    test_accuracy_lst_2 = []
+    train_loss_lst_4 = []
+    test_loss_lst_4 = []
+    train_accuracy_lst_4 = []
+    test_accuracy_lst_4 = []
 
     for epoch in range(epochs):
         # Shuffle the dataset at the beginning of each epoch
@@ -33,23 +39,40 @@ def main():
             X_batch = shuffled_images[i:i + batch_size]
             Y_batch = shuffled_labels[i:i + batch_size]
 
-            # Forward and backward propagation on the mini-batch
+            # two layer model
             Z1, A1 = two_layer_model.forward(X_batch)
             dW1, db1 = two_layer_model.backward(X_batch, Y_batch, Z1, A1)
 
-            # Update parameters
             two_layer_model.update_params(dW1, db1, learning_rate)
 
-        train_loss_lst.append(compute_loss(train_images, train_labels, two_layer_model))
-        test_loss_lst.append(compute_loss(test_images, test_labels, two_layer_model))
+            # four layer model
+            Z1, A1, Z2, A2, Z3, A3 = four_layer_model.forward(X_batch)
+            dW1, db1, dW2, db2, dW3, db3 = four_layer_model.backward(X_batch, Y_batch, Z1, A1, Z2, A2, Z3, A3)
 
-        train_accuracy_lst.append(compute_accuracy(train_images, train_labels, two_layer_model))
-        test_accuracy_lst.append(compute_accuracy(test_images, test_labels, two_layer_model))
-        print_epoch_result(epoch, test_accuracy_lst, test_loss_lst, train_accuracy_lst, train_loss_lst)
+            four_layer_model.update_params(dW1, db1, dW2, db2, dW3, db3, learning_rate)
 
-    plot_result_figure(train_loss_lst, test_loss_lst, train_accuracy_lst, test_accuracy_lst,
-                       "Model Performance",
-                       "./task2/result.png")
+        # two layer model
+        train_loss_lst_2.append(compute_loss(train_images, train_labels, two_layer_model))
+        test_loss_lst_2.append(compute_loss(test_images, test_labels, two_layer_model))
+
+        train_accuracy_lst_2.append(compute_accuracy(train_images, train_labels, two_layer_model))
+        test_accuracy_lst_2.append(compute_accuracy(test_images, test_labels, two_layer_model))
+        print_epoch_result(epoch, test_accuracy_lst_2, test_loss_lst_2, train_accuracy_lst_2, train_loss_lst_2)
+
+        # four layer model
+        train_loss_lst_4.append(compute_loss(train_images, train_labels, four_layer_model))
+        test_loss_lst_4.append(compute_loss(test_images, test_labels, four_layer_model))
+
+        train_accuracy_lst_4.append(compute_accuracy(train_images, train_labels, four_layer_model))
+        test_accuracy_lst_4.append(compute_accuracy(test_images, test_labels, four_layer_model))
+        print_epoch_result(epoch, test_accuracy_lst_4, test_loss_lst_4, train_accuracy_lst_4, train_loss_lst_4)
+
+    plot_result_figure(train_loss_lst_2, test_loss_lst_2, train_accuracy_lst_2, test_accuracy_lst_2,
+                       "Two Layer Model Performance",
+                       "./task2/twoLayerModel.png")
+    plot_result_figure(train_loss_lst_4, test_loss_lst_4, train_accuracy_lst_4, test_accuracy_lst_4,
+                       "Four Layer Model Performance",
+                       "./task2/fourLayerModel.png")
 
 
 if __name__ == '__main__':

4df14e53256589dc7bade893e93a2b1a0d0534f5
HuoJia <hjia@curietech.net>
2024-04-16 20:12:55 +0800

    TaskA: 训练完成基础模型，准确率: 91.56%

---
+++ adversarial-training/taskA/test_model.py
@@ -0,0 +1,37 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from taskA.model.model import SimpleCNN
+
+
+def test_model(test_loader, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    return accuracy
+
+
+if __name__ == '__main__':
+    model = SimpleCNN()  # 创建模型实例
+    model.load_state_dict(torch.load('./model/model_params.pth'))  # 加载保存的模型参数
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+    test_dataset = datasets.FashionMNIST('./data', train=False, download=False, transform=transform)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    accuracy = test_model(test_loader, device)
+    print(f"Model Accuracy on test dataset: {accuracy}%")  # 91.56%
---
+++ adversarial-training/taskA/train.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from taskA.model.model import SimpleCNN
+
+
+# 模型训练函数
+def train_model(epochs, log_step, model, train_loader, device):
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.CrossEntropyLoss()
+
+    # 训练模型
+    model.train()
+    for epoch in range(epochs):  # 训练10个epoch
+        print(f"Epoch {epoch + 1}")
+        idx = 0
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            idx += 1
+            if idx % log_step == 0:
+                print(f"    Loss: {loss}")
+
+
+def test_model(test_loader, device):
+    # 验证模型
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    return accuracy
+
+
+if __name__ == '__main__':
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    print("Start load dataset")
+    train_dataset = datasets.FashionMNIST('./data', train=True, download=False, transform=transform)
+    test_dataset = datasets.FashionMNIST('./data', train=False, download=False, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    print("Load dataset finish ")
+
+    model = SimpleCNN()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_model(15, 30, model, train_loader, device)
+    accuracy = test_model(test_loader, device)
+    print(f'Accuracy: {accuracy}%')
+    torch.save(model.state_dict(), './model/model_params.pth')

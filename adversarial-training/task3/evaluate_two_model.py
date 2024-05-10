8a7abc31f7f13b36c4e2e7a022b0ed5e7ff4871a
HuoJia <hjia@curietech.net>
2024-05-10 16:03:29 +0800

    完成所有code及doc

--- adversarial-training/task3/evaluate_two_model.py
+++ adversarial-training/task3/evaluate_two_model.py
@@ -5,6 +5,8 @@
 4. 在新旧模型上分别进行白盒攻击，对比准确率
 5. 在新旧模型上分别进行黑盒攻击，对比准确率
 """
+import pickle
+
 import numpy as np
 import torch
 from torch import nn
@@ -13,6 +15,7 @@ from torchvision import datasets
 from torchvision.transforms import transforms
 from tqdm import tqdm
 
+from task3.model.custom_dataset import CustomDataset
 from task3.model.model import SimpleCNN
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -46,7 +49,7 @@ def load_white_attack_data():
 
 def white_attack(model, data_loader):
     target_classes = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 8: 9, 9: 0}
-    iterations = 30
+    iterations = 15
     model.eval()
     successful_samples = []
     attempts = 0
@@ -83,40 +86,67 @@ def white_attack(model, data_loader):
             successful_samples.append((origin_image, label, image, target_label))
 
         attempts += 1
+    return successful_samples, attempts
+
+
+def get_white_attack_success_rate(model, data_loader):
+    successful_samples, attempts = white_attack(model, data_loader)
     attack_success_rate = 100 * len(successful_samples) / attempts
     return attack_success_rate
 
 
 def load_black_attack_data(file_name):
-    data = np.load(file_name)
-    original_images = data['original_images']
-    original_labels = data['original_labels']
-    perturbed_images = data['perturbed_images']
-    perturbed_labels = data['perturbed_labels']
-    return original_images, original_labels, perturbed_images, perturbed_labels
+    # load 1k dataset
+    with open('data/correct_1k.pkl', 'rb') as file:
+        data = pickle.load(file)
+    images, labels = data[0], data[1]
+
+    images = np.array(images, dtype='float32') / 255.0
+    images = images.reshape(-1, 28, 28)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+    dataset = CustomDataset(images, labels, transform=transform)
+    data_loader = DataLoader(dataset, batch_size=1, shuffle=True)
+    return data_loader
+    # data = np.load(file_name)
+    # original_images = data['original_images']
+    # original_labels = data['original_labels']
+    # perturbed_images = data['perturbed_images']
+    # perturbed_labels = data['perturbed_labels']
+    # return original_images, original_labels, perturbed_images, perturbed_labels
 
 
 def black_attack(model, data):
     model.eval()
     attack_success_list = []
 
-    for original_image, original_label, perturbed_image, perturbed_label in zip(*data):
+    for original_image, original_label, perturbed_image, perturbed_label in data:
         with torch.no_grad():
-            outputs = model(torch.from_numpy(perturbed_image))
+            outputs = model(perturbed_image)
             _, predicted = torch.max(outputs.data, 1)
 
         if predicted.item() == perturbed_label.item():
             result = (original_image, original_label, perturbed_image, perturbed_label)
             attack_success_list.append(result)
+    return attack_success_list
 
-    attack_success_rate = len(attack_success_list) / len(data[0]) * 100
+
+def get_black_attack_rate(model, data):
+    # get success white attack sample
+    successful_white_attack_samples, _ = white_attack(model, data)
+
+    # black attack
+    attack_success_list = black_attack(model, successful_white_attack_samples)
+    attack_success_rate = len(attack_success_list) / len(data) * 100
     return attack_success_rate
 
 
 def test_two_model(func, model_without_attack, model_with_attack, data, desc):
     print(f"\n{'=' * 20} Start test model about {desc} {'=' * 20}")
     model_without_attack_accuracy = func(model_without_attack, data)
-    print(f"Model trained without attack dataset about {desc}: {model_without_attack_accuracy}%")
+    print(f"\nModel trained without attack dataset about {desc}: {model_without_attack_accuracy}%")
 
     model_with_attack_accuracy = func(model_with_attack, data)
     print(f"Model trained with attack dataset about {desc}: {model_with_attack_accuracy}%")
@@ -148,17 +178,17 @@ if __name__ == '__main__':
                    "model accuracy on test dataset")
 
     # 测试两个模型在白盒攻击上的成功率
-    # white_attack_data = load_white_attack_data()
-    # test_two_model(white_attack,
-    #                model_without_attack,
-    #                model_with_attack,
-    #                white_attack_data,
-    #                "attack success rate using while attack")
+    white_attack_data = load_white_attack_data()
+    test_two_model(get_white_attack_success_rate,
+                   model_with_attack,
+                   model_without_attack,
+                   white_attack_data,
+                   "attack success rate using white attack")
 
     # 测试两个模型在黑盒攻击上的成功率
     black_attack_data = load_black_attack_data("data/black_attack_data_896.npz")
-    test_two_model(black_attack,
-                   model_without_attack,
+    test_two_model(get_black_attack_rate,
                    model_with_attack,
+                   model_without_attack,
                    black_attack_data,
                    "attack success rate using black attack")
